5825b3dab41bb33f5606d2625fb8e35c95140ff5
VikVik02 <vikstar21997@mail.ru>
2019-03-13 18:52:47 +1000

    Сделала задания с 1 по 9. Применила рекурсию в 3, 4 и 7 задачах

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,15 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+def recursion(num):
+    if num > 0:
+        s = str(num % 10)
+        s += str(recursion(num // 10))
+        return s
+    elif num == 0:
+        return ''
+
+
+num = int(input("Введите число"))
+print(recursion(num))
--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,25 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+
+quant = int(input("Введите количество чисел"))
+
+max_num = 0
+max_sum = 0
+
+while quant != 0:
+    num = int(input("Введите число"))
+    while num != 0:
+        summ = 0
+        maxx = num
+        while num > 0:
+            summ += num % 10
+            num //= 10
+        if summ > max_sum:
+            max_sum = summ
+            max_num = maxx
+    quant -= 1
+
+print(f"Число {max_num} имеет максимальную сумму {max_sum}")
+
+
